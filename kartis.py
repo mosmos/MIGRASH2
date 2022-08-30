da2efc02a205452e220f1d6a93ad197c45eb7119
mosmos <moshe.yaniv@gmail.com>
2022-08-30 19:19:08 +0300

    dict

--- kartis.py
+++ kartis.py
@@ -1,29 +1,20 @@
 import time
 import settings
-print(time.ctime())
-
 import arcpy
 
-geodataexist=arcpy.Exists(settings.gdbpath)
-
-if geodataexist == True:
+if arcpy.Exists(settings.gdbpath):
       arcpy.Delete_management(settings.gdbpath)
       print("deleted old GDB")
 else:
       print("no need to delete")
 
-arcpy.CreateFileGDB_management(settings.datapath,settings.gdbname)
-print("created new GDB")
+gdb = arcpy.CreateFileGDB_management(settings.datapath,settings.gdbname)
+print(f"created new GDB:{gdb}")
 
 for layer in settings.layers2:
-      print (layer)
-      arcpy.FeatureClassToFeatureClass_conversion(\
-            settings.connectionpath+layer,\
-            settings.gdbpath,\
-            settings.layers2[layer])
-      
-
-print(time.ctime())
+      print (f"copying layer:{layer}")
+      arcpy.FeatureClassToFeatureClass_conversion(settings.connectionpath+layer,settings.gdbpath,settings.layers2[layer])
+      print(time.ctime())
 
 
 
--- settings.py
+++ settings.py
@@ -2,14 +2,29 @@ gdbpath="C:/TEMP/GDATA/LAYERSFORPROCESS.gdb"
 datapath="C:/TEMP/GDATA"
 gdbname="LAYERSFORPROCESS.gdb"
 connectionpath="C:/TEMP/connectionfile/dgt-sde01db720.sde/"
-layers=["db720.dbo.KD_TG_CHELKOT_BAALUT",
-"db720.dbo.es_tg_raash_45m",
-"db720.dbo.ta_tg_biuv_kav",
-"db720.dbo.kl_tg_tree_all"]
-layersnickname=["migrashim","raash","biuv","etzim"]
-
-layers2 = {"db720.dbo.KD_TG_CHELKOT_BAALUT","migrashim"}
-
+layers2 = {"db720.dbo.KD_TG_CHELKOT_BAALUT":"migrashim",
+    "db720.dbo.es_tg_raash_45m":"raash_45m",
+    "db720.dbo.ta_tg_biuv_kav":"biuv_kav",
+    "db720.dbo.kl_tg_tree_all":"tree_all",
+    "db720.dbo.kd_tg_gushim":"gushim",
+    "db720.dbo.kd_tg_chelkot":"_chelkot",
+    "db720.dbo.kt_tg_ktovot":"ktovot",
+    "db720.dbo.nh_tg_baalut_iria":"baalut_iria",
+    "db720.dbo.es_tg_contaminated_site":"contaminated_site",
+    "db720.dbo.kl_tg_ezor_atikot":"ezor_atikot",
+    "db720.dbo.ta_tg_flooding_area":"flooding_area",
+    "db720.dbo.ta_tg_flooding_ayalon":"flooding_ayalon",
+    "db720.dbo.ta_tg_radius_magen_mechonim":"magen_mechonim",
+    "db720.dbo.es_tg_tahanot_delek_buffers":"delek_buffers",
+    "db720.dbo.kl_tg_mivnim_leshimur":"mivnim_leshimur",
+    "db720.dbo.kl_tg_ir_levana":"ir_levana",
+    "db720.dbo.kl_tg_mivnim_leshimur_brutalism":"mivnim_leshimur_brutalism",
+    "db720.dbo.kl_tg_mivne_mesukan":"mivne_mesukan",
+    "db720.dbo.ta_tg_hashmal_ili_ln":"hashmal_ili_ln",
+    "db720.dbo.ta_tg_hashmal_tat_karkai_ln":"hashmal_tat_karkai_ln",
+    "db720.dbo.kl_tg_trees_achzaka":"trees_achzaka",
+    "db720.dbo.kl_tg_ginot_klavim":"ginot_klavim",
+    "db720.dbo.kl_tg_ginot_kehilatiyot":"kehilatiyot"}
 
 
 
