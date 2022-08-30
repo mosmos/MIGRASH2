c7454db7320b61adf896af67b103cd97a63f5efe
SZ <zini_s@mail.tel-aviv.gov.il>
2022-08-30 18:48:44 +0300

    settings

---
+++ settings.py
@@ -0,0 +1,9 @@
+gdbpath="C:/TEMP/GDATA/LAYERSFORPROCESS.gdb"
+datapath="C:/TEMP/GDATA"
+gdbname="LAYERSFORPROCESS.gdb"
+connectionpath="C:/TEMP/connectionfile/dgt-sde01db720.sde/"
+layers=["db720.dbo.KD_TG_CHELKOT_BAALUT",
+"db720.dbo.es_tg_raash_45m",
+"db720.dbo.ta_tg_biuv_kav",
+"db720.dbo.kl_tg_tree_all"]
+layersnickname=["migrashim","raash","biuv","etzim"]
