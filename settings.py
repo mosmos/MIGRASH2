3c5767d95a27c9977f0e922fffcd25d265bcc495
SZ <zini_s@mail.tel-aviv.gov.il>
2022-09-11 11:15:05 +0300

    with sql layer filter

--- settings.py
+++ settings.py
@@ -2,29 +2,34 @@ gdbpath="C:\\TEMP\\GDATA\\LAYERSFORPROCESS.gdb"
 datapath="C:\\TEMP\\GDATA"
 gdbname="LAYERSFORPROCESS.gdb"
 connectionpath="C:\\TEMP\\connectionfile\\dgt-sde01db720.sde\\"
-layers2 = {"db720.dbo.KD_TG_CHELKOT_BAALUT":"migrashim",
-    "db720.dbo.es_tg_raash_45m":"raash_45m",
-    "db720.dbo.ta_tg_biuv_kav":"biuv_kav",
-    "db720.dbo.kl_tg_tree_all":"tree_all",
-    "db720.dbo.kd_tg_gushim":"gushim",
-    "db720.dbo.kd_tg_chelkot":"_chelkot",
-    "db720.dbo.kt_tg_ktovot":"ktovot",
-    "db720.dbo.nh_tg_baalut_iria":"baalut_iria",
-    "db720.dbo.es_tg_contaminated_site":"contaminated_site",
-    "db720.dbo.kl_tg_ezor_atikot":"ezor_atikot",
-    "db720.dbo.ta_tg_flooding_area":"flooding_area",
-    "db720.dbo.ta_tg_flooding_ayalon":"flooding_ayalon",
-    "db720.dbo.ta_tg_radius_magen_mechonim":"magen_mechonim",
-    "db720.dbo.es_tg_tahanot_delek_buffers":"delek_buffers",
-    "db720.dbo.kl_tg_mivnim_leshimur":"mivnim_leshimur",
-    "db720.dbo.kl_tg_ir_levana":"ir_levana",
-    "db720.dbo.kl_tg_mivnim_leshimur_brutalism":"mivnim_leshimur_brutalism",
-    "db720.dbo.kl_tg_mivne_mesukan":"mivne_mesukan",
-    "db720.dbo.ta_tg_hashmal_ili_ln":"hashmal_ili_ln",
-    "db720.dbo.ta_tg_hashmal_tat_karkai_ln":"hashmal_tat_karkai_ln",
-    "db720.dbo.kl_tg_trees_achzaka":"trees_achzaka",
-    "db720.dbo.kl_tg_ginot_klavim":"ginot_klavim",
-    "db720.dbo.kl_tg_ginot_kehilatiyot":"kehilatiyot"}
+layers2 = {"db720.dbo.KD_TG_CHELKOT_BAALUT":{"alias":"migrashim","sql":None},
+    "db720.dbo.es_tg_raash_45m":{"alias":"raash_45m","sql":None},
+    "db720.dbo.ta_tg_biuv_kav":{"alias":"biuv_kav","sql":None},
+    "db720.dbo.kl_tg_tree_all":{"alias":"tree_all","sql":None},
+    "db720.dbo.kd_tg_gushim":{"alias":"gushim","sql":None},
+    "db720.dbo.kd_tg_chelkot":{"alias":"chelkot","sql":None},
+    "db720.dbo.kt_tg_ktovot":{"alias":"ktovot","sql":None},
+    "db720.dbo.nh_tg_baalut_iria":{"alias":"baalut_iria","sql":None},
+    "db720.dbo.es_tg_contaminated_site":{"alias":"contaminated_site","sql":None},
+    "db720.dbo.kl_tg_ezor_atikot":{"alias":"ezor_atikot","sql":None},
+    "db720.dbo.ta_tg_flooding_area":{"alias":"flooding_area","sql":None},
+    "db720.dbo.ta_tg_flooding_ayalon":{"alias":"flooding_ayalon","sql":None},
+    "db720.dbo.ta_tg_radius_magen_mechonim":{"alias":"mmagen_mechonim","sql":None},
+    "db720.dbo.es_tg_tahanot_delek_buffers":{"alias":"tahanot_delek_buffers","sql":None},
+    "db720.dbo.kl_tg_mivnim_leshimur":{"alias":"mivnim_leshimur","sql":None},
+    "db720.dbo.kl_tg_ir_levana":{"alias":"ir_levana","sql":None},
+    "db720.dbo.kl_tg_mivnim_leshimur_brutalism":{"alias":"mivnim_leshimur_brutalism","sql":None},
+    "db720.dbo.kl_tg_mivne_mesukan":{"alias":"mivne_mesukan","sql":None},
+    "db720.dbo.ta_tg_hashmal_ili_ln":{"alias":"hashmal_ili_ln","sql":None},
+    "db720.dbo.ta_tg_hashmal_tat_karkai_ln":{"alias":"hashmal_tat_karkai_ln","sql":None},
+    "db720.dbo.kl_tg_trees_achzaka":{"alias":"trees_achzaka","sql":None},
+    "db720.dbo.kl_tg_ginot_klavim":{"alias":"ginot_klavim","sql":None},
+    "db720.DBO.yk_tg_migrashim":{"alias":"yeudei_karka", "sql":"k_yeud_rashi=1005"},
+    "db720.dbo.kl_tg_ginot_kehilatiyot":{"alias":"ginot_kehilatiyot","sql":None}}
+
+
+
+
 
 
 
